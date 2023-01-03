0d2effc23b1a9d58951ab886827f6e8c0ec0ccf8
Platon <97834402+Master-Stroke@users.noreply.github.com>
2023-01-03 21:22:35 +0200

    Update 1banner.py

--- plugins/1banner.py
+++ plugins/1banner.py
@@ -5,7 +5,8 @@ from plugins.settings.main_settings import version
 from prefix import my_prefix
 prefix = my_prefix()
 
-os.system("cls" if os.name == "nt" else "clear")
+if not os.name == "nt":
+  os.system("cls" if os.name == "nt" else "clear")
 print(f"""[blue]
  ____  _                _    _   _               ____        _   
 / ___|| |__   __ _ _ __| | _| | | |___  ___ _ __| __ )  ___ | |_ 
