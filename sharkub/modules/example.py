7eb05feef2bea4d0837f13567e788a36c5774ec5
Platon <97834402+Master-Stroke@users.noreply.github.com>
2023-02-05 03:50:39 +0200

    Update example.py

--- sharkub/modules/example.py
+++ sharkub/modules/example.py
@@ -1,21 +1,12 @@
 from pyrogram import Client, filters
-from time import sleep
 from ..settings.main_settings import module_list, file_list
 
 from ..settings.prefix import my_prefix
 prefix = my_prefix()
 
-
-@Client.on_message(filters.command("redly", prefixes=prefix) & filters.me)
-async def redly_spam(client, message):
-  while True:  
-    await message.edit(f"+rep")
-    sleep(0.5)
-    await message.edit(f"-rep")
-    sleep(0.5)
-    await message.edit(f"спс")
-    sleep(0.5)
-
+@Client.on_message(filters.command("example_edit", prefixes=prefix) & filters.me)
+async def example_edit(client, message):
+    await message.edit("<code>This is an example module</code>")
 
 module_list["Example"] = {
 "example_edit": "Description",
